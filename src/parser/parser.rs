feac2e431fb9f4f48611b1b897f5f04a2f30c8cb
Mikita Slabysh <slabysh2015@gmail.com>
2026-04-27 22:15:48 -0400

    started working on parser class

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,6 +1,10 @@
+pub struct Parser {
+  input : String,
+}
+
 impl Parser {
-    pub fn new() -> Self {
-        Parser {}
+    pub fn new(input: String) -> Self {
+        Parser { input }
     }
 
     pub fn parse(&self, code: &str) -> Result<(), String> {
